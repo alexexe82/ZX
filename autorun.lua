ffe7057a8a715b41a5e3d34e2bc4b1d7d28b887e
alexexe82 <alexexe82@mail.ru>
2019-01-09 22:59:11 +0600

    alexexe82

--- autorun.lua
+++ autorun.lua
@@ -1,1 +1,3 @@
-local success, reason = pcall(loadfile("zx.lua")); if not success then print("Ошибка: " .. tostring(reason)) end
+local shell = require("shell")
+shell.execute("cd //")
+local success, reason = pcall(loadfile("/zx.lua")); if not success then print("Ошибка: " .. tostring(reason)) end
